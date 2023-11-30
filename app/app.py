2eed7bffaf047514bb7fdbbc5a656d1fb5b8f9ec
Matias Martin Murad Pretz <30426732+matipretz@users.noreply.github.com>
2023-11-30 20:10:04 -0300

    clean up

--- app/app.py
+++ app/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 CORS(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:@localhost/login"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:@localhost/pysword$pysword_db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
---
+++ app/schema.sql
@@ -0,0 +1,29 @@
+DROP TABLE IF EXISTS `users`;
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` INTEGER NULL AUTO_INCREMENT PRIMARY KEY,
+  `username` VARCHAR(64) NULL DEFAULT NULL,
+  `email` VARCHAR(120) NULL DEFAULT NULL,
+  `password_hash` VARCHAR(128) NULL DEFAULT NULL
+);
+
+DROP TABLE IF EXISTS `entradas`;
+
+CREATE TABLE `entradas` (
+  `id` INTEGER NULL AUTO_INCREMENT PRIMARY KEY,
+  `servicio` VARCHAR(64) NULL DEFAULT NULL,
+  `usuario` VARCHAR(120) NULL DEFAULT NULL,
+  `contrasena` VARCHAR(128) NULL DEFAULT NULL,
+  `id_users` INTEGER NULL DEFAULT NULL,
+  FOREIGN KEY (`id_users`) REFERENCES `users` (`id`)
+);
+
+-- ---
+-- Test Data
+-- ---
+
+-- INSERT INTO `users` (`id`,`username`,`email`,`password_hash`) VALUES
+-- ('','','','');
+-- INSERT INTO `entradas` (`id`,`servicio`,`usuario`,`contrasena`,`id_users`) VALUES
+-- ('','','','','');
